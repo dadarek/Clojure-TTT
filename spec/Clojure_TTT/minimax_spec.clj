8bd95cc2cbcbe058be65799fc6e086ff44a63930
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-23 15:44:40 -0500

    Basic tests.

--- spec/Clojure_TTT/minimax_spec.clj
+++ spec/Clojure_TTT/minimax_spec.clj
@@ -3,8 +3,38 @@
   (:use [speclj.core]))
 
 (describe "Minimax"
-  (it "Dummy tests"
-    (should= 6 (* 3 2)))
+  (it "Scores full board as 0's"
+    (let [expected-scores
+            '(0  0  0
+              0  0  0
+              0  0  0)
+          board
+            '(:x :o :x
+              :x :o :o
+              :o :x :x)]
+      (should= expected-scores (value-each-square board))))
+
+  (it "Scores winning square as 1"
+    (let [expected-scores
+            '(0  0  0
+              0  0  0
+              0  0  1)
+          board
+            '(:x :o :x
+              :x :x :o
+              :o :o nil)]
+      (should= expected-scores (value-each-square board))))
+
+  (it "Scores losing square as 1"
+    (let [expected-scores
+            '(0  0  0
+              0  0  0
+              0  0  0)
+          board
+            '( :x :o :x
+              nil :x :o
+               :o :x :o)]
+      (should= expected-scores (value-each-square board))))
 )
 
 (run-specs)
--- src/Clojure_TTT/minimax.clj
+++ src/Clojure_TTT/minimax.clj
@@ -1,2 +1,10 @@
 (ns clojure_ttt.minimax)
 
+(defn value-square [square board]
+  (if (= nil square) 1 0))
+
+(defn value-each-square [board]
+  (for [square board]
+    (value-square square board)
+    )
+)
