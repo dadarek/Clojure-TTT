e87a04c4b4c9bfefbd2f22f6454e953eb9e9768a
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-23 11:12:45 -0500

    More complex tests.

--- spec/Clojure_TTT/minimax_spec.clj
+++ spec/Clojure_TTT/minimax_spec.clj
@@ -1,12 +1,31 @@
 (ns clojure_ttt.minimax-spec
   (:use [speclj.core]))
 
-(defn true-or-false []
-  true)
+(defn equal-5? [n]
+  (= 5 n))
 
-(describe "truthiness"
-  (it "tests something"
-    (should (true-or-false))))
+(describe "Understanding speclj"
+  (before-all 
+    )
+
+  (after-all
+    )
+
+  (before
+    )
+
+  (after
+    )
+
+  (it "Does some stuff"
+    (should (equal-5? (+ 4 1))))
+
+  (it "Does some more stuff"
+    (should-not (equal-5? (+ 4 3))))
+
+  (it "Tests against 6"
+    (should= 6 (* 3 2)))
+)
 
 (run-specs)
 
