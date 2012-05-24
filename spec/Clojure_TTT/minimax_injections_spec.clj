b62e441cc2b83af6d921b73682db583b400202b4
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-24 09:48:38 -0500

    Adding some board utilities for determining a players squares.

--- spec/Clojure_TTT/minimax_injections_spec.clj
+++ spec/Clojure_TTT/minimax_injections_spec.clj
@@ -4,7 +4,7 @@
 
 (describe "Minimax Injections"
   (context "Wins"
-    (it "Recognizes winning move."
+    (it "Recognizes winning move (row 1)."
       (let [context { :player :x
                       :board '(:x :x nil
                                :o :o nil
@@ -14,11 +14,26 @@
     )
     (it "Recognizes non-winning move."
       (let [context { :player :x
-                      :board '(:x :x nil
-                               :o :o nil
+                      :board '(:x :o nil
+                               :o :x nil
                                nil nil nil)}]
-        (should-not (wins? 6 context))
+        (should-not (wins? 3 context))
       )
+    )
+  )
+  ; TODO: Move this out into board utils or something
+  (context "Player squares"
+    (it "finds player x squares"
+      (should= #{1 2 8} (player-squares '(:x :x nil
+                                        nil nil :o
+                                        nil :x nil)
+                                       :x))
+    )
+    (it "finds player o squares"
+      (should= #{5 9} (player-squares '(:x :x nil
+                                        nil :o nil
+                                        nil :x :o)
+                                       :o))
     )
   )
 )
--- src/Clojure_TTT/minimax_injections.clj
+++ src/Clojure_TTT/minimax_injections.clj
@@ -1,6 +1,19 @@
-(ns clojure_ttt.minimax_injections)
+(ns clojure_ttt.minimax_injections
+  (:use clojure.set)
+  (:use clojure_ttt.rules))
 
+
+(defn player-squares [board player]
+  (set
+    (filter #(not= nil %)
+      (map (fn [i] (if (= player (nth board i)) (inc i))) (range 0 9))
+    )
+  )
+)
 
 (defn wins? [square context]
-  (= 3 square)
+  (winner? (conj (player-squares
+                   (:board context)
+                   (:player context))
+           square))
 )
