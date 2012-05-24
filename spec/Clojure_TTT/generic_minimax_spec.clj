7c85b0008f03ae830377a9ddd393348c55fc2f9a
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-24 08:50:31 -0500

    Refactoring

--- spec/Clojure_TTT/generic_minimax_spec.clj
+++ spec/Clojure_TTT/generic_minimax_spec.clj
@@ -4,43 +4,39 @@
 
 (describe "Generic Minimax"
   (it "Returns 0 on moves that tie"
-    (should= '(0 0 0)
-              (score
-                #{0 1 2}
-                :context
-                (fn [move context] false)
-                (fn [move context] true)
-                nil
+    (should= '(0 0 0) (score
+                        #{0 1 2}
+                        nil
+                        (fn [move context] false)
+                        (fn [move context] true)
+                        nil
       )
     )
   )
   (it "Returns 1 for moves that win"
-    (should= '(1 1 1)
-              (score
-                #{0 1 2}
-                :context
-                (fn [move context] true)
-                nil
-                nil
+    (should= '(1 1 1) (score
+                        #{0 1 2}
+                        nil
+                        (fn [move context] true)
+                        nil
+                        nil
       )
     )
   )
-  (it "Returns sum of children moves that don't win or tie"
-    (should= '(2 2 2)
-              (score
-                #{0 1 2}
-                :context
-                (fn [move context] false)
-                (fn [move context] false)
-                (fn [move context] 2)
+  (it "Returns sum of children for moves that don't win or tie"
+    (should= '(2 2 2) (score
+                        #{0 1 2}
+                        nil
+                        (fn [move context] false)
+                        (fn [move context] false)
+                        (fn [move context] 2)
       )
     )
   )
   (it "Passes context to injected functions"
-      ; force this without an external should
-    (should= '(3 3 3)
+    (should= '(3)
               (score
-                #{0 1 2}
+                [0]
                 :context
                 (fn [move context] (should= :context context) false)
                 (fn [move context] (should= :context context) false)
