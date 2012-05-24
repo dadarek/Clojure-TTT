782384e52ce1f97f828a539b07f0fa94d5d0d95c
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-24 08:15:24 -0500

    Adding last step of minimax.

--- spec/Clojure_TTT/generic_minimax_spec.clj
+++ spec/Clojure_TTT/generic_minimax_spec.clj
@@ -4,26 +4,41 @@
 
 (describe "Generic Minimax"
   (it "Returns 0 on moves that tie"
-    (should= '(0 0 0) 
-              (score 
-                #{0 1 2} 
+    (should= '(0 0 0)
+              (score
+                #{0 1 2}
                 nil
                 nil
                 (fn [move board player] false)
                 (fn [move board player] true)
+                nil
       )
     )
   )
-
   (it "Returns 1 for moves that win"
     (let [board '(0 1 0)]
-      (should= '(0 1 0) 
-                (score 
-                  #{0 1 2} 
+      (should= '(1 1 1)
+                (score
+                  #{0 1 2}
                   board
                   nil
-                  (fn [move b player] (= 1 (nth b move)))
-                  (fn [move b player] (= -1 (nth b move)))
+                  (fn [move b player] true)
+                  (fn [move b player] false)
+                  nil
+        )
+      )
+    )
+  )
+  (it "Returns sum of children moves that don't win or tie"
+    (let [board '(0 1 0)]
+      (should= '(2 2 2)
+                (score
+                  #{0 1 2}
+                  board
+                  nil
+                  (fn [move b player] false)
+                  (fn [move b player] false)
+                  (fn [move b player] 2)
         )
       )
     )
--- src/Clojure_TTT/generic_minimax.clj
+++ src/Clojure_TTT/generic_minimax.clj
@@ -1,13 +1,15 @@
 (ns clojure_ttt.generic_minimax)
 
 
-(defn score [moves board player won? tie?] 
-  (map 
-    (fn [move] 
-      (if (won? move board player) 1 
-        (if (tie? move board player) 0 0)
+(defn score [moves board player won? tie? sum_of_children]
+  (map
+    (fn [move]
+      (if (won? move board player) 1
+        (if (tie? move board player) 0
+          (sum_of_children move board player)
+        )
       )
-    ) 
+    )
     moves
   )
 )
