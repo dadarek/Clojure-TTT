32925910e4fe09a830d345a1b780450279fee350
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-25 14:08:05 -0500

    Cleanup.

--- src/Clojure_TTT/generic_minimax.clj
+++ src/Clojure_TTT/generic_minimax.clj
@@ -1,16 +1,17 @@
 (ns clojure_ttt.generic_minimax)
 
 
-(defn score [moves context wins? ties? next-context]
-  (let [score-move (fn [move]
-      (if (wins? move context) 1
-      (if (ties? move context) 0
-      (- (reduce + 
-            (score
-               (disj moves move)
-               (next-context move context)
-               wins?
-               ties?
-               next-context))))))]
+(defn score [moves context wins? ties? get-child-context-of]
+  (let [child-moves-of (fn [move] (disj moves move))
+        score-move (fn [move]
+                      (if (wins? move context) 1
+                      (if (ties? move context) 0
+                      (- (reduce +
+                            (score
+                               (child-moves-of move)
+                               (get-child-context-of move context)
+                               wins?
+                               ties?
+                               get-child-context-of))))))]
     (map score-move moves)))
       
