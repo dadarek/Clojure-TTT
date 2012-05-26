42109039091b670f93c49aae0c271a8523c21223
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-26 10:33:41 -0500

    One-lining this guy - it's pretty clear what's going on.

--- src/Clojure_TTT/generic_minimax.clj
+++ src/Clojure_TTT/generic_minimax.clj
@@ -4,12 +4,7 @@
 (defn score [moves context wins? ties? get-child-context-of]
   (let [score-child-moves-of
           (fn [move]
-            (score
-              (disj moves move)
-              (get-child-context-of move context)
-              wins?
-              ties?
-              get-child-context-of))
+            (score (disj moves move) (get-child-context-of move context) wins? ties? get-child-context-of))
         score-single-move
           (fn [move]
             (cond
