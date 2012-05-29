56e6c8a109e44619959a69cfc51cc60fe54b5d5b
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-29 14:41:04 -0500

    Making sure runner stops game when somebody wins.

--- spec/Clojure_TTT/runner_spec.clj
+++ spec/Clojure_TTT/runner_spec.clj
@@ -15,6 +15,13 @@
                :x :o :o
                :o :x :x)
               (run (MockPlayer. '(1 3 4 8 9) :x)
-                   (MockPlayer. '(2 5 6 7) :o)))))
+                   (MockPlayer. '(2 5 6 7) :o))))
+
+  (it "Stops when somebody wins"
+    (should= '(:x  :x  :x
+               :o  :o  nil
+               nil nil nil)
+              (run (MockPlayer. '(1 2 3) :x)
+                   (MockPlayer. '(4 5) :o)))))
 
 (run-specs)
--- src/Clojure_TTT/runner.clj
+++ src/Clojure_TTT/runner.clj
@@ -1,5 +1,6 @@
 (ns clojure_ttt.runner
-  (:use [clojure_ttt.board_utilities]))
+  (:use [clojure_ttt.board_utilities])
+  (:use [clojure_ttt.rules]))
 
 (defprotocol Player
   (next-move [p board])
@@ -10,12 +11,15 @@
 
 (defn run [x o]
   (loop [context {:board '(nil nil nil nil nil nil nil nil nil) :player :x}]
-    (if (full? (:board context))
-      (:board context)
-      (let [current-player (if (= :x (:player context)) x o)
-            next-move (next-move current-player (:board context))
-            new-board (take-square next-move context)
-            new-player (next-player context)
-            new-context {:board new-board :player new-player} ]
-        (recur new-context)))))
+    (let [board (:board context)
+          x-won (winner? (player-squares board :x))
+          o-won (winner? (player-squares board :o))]
+      (if (or (full? board) x-won o-won)
+        board
+        (let [current-player (if (= :x (:player context)) x o)
+              next-move (next-move current-player board)
+              new-board (take-square next-move context)
+              new-player (next-player context)
+              new-context {:board new-board :player new-player} ]
+          (recur new-context))))))
 
