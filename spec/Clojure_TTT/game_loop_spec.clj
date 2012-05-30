46b234ed6de824eecc9b504602c45eec8e68cb8b
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-30 14:56:45 -0500

    Making sure correct player is allowed to go first.

--- spec/Clojure_TTT/game_loop_spec.clj
+++ spec/Clojure_TTT/game_loop_spec.clj
@@ -1,5 +1,7 @@
 (ns clojure_ttt.game_loop-spec
   (:use [clojure_ttt.game_loop])
+  (:use [clojure_ttt.runner])
+  (:import [clojure_ttt.human_player HumanPlayer])
   (:use [speclj.core]))
 
 (defn inc-counter [counter]
@@ -17,16 +19,31 @@
       (inc-counter go-first-counter)
       result)))
 
-(describe "Game Loop"
-  (it "Plays until play-again returns false"
-    (def counter (ref 0))
-    (do (play (MockLoopUI. counter (ref 0) [true true true false]))
-    (should= 4 @counter)))
+(with-redefs [run (fn [& args] true)]
+  (describe "Game Loop"
+    (it "Plays until play-again returns false"
+      (def counter (ref 0))
+      (def ui (MockLoopUI. counter (ref 0) [true true true false]))
+      (do (play nil nil ui))
+      (should= 4 @counter))
 
-  (it "Asks if player wants to go first"
-    (def counter (ref 0))
-    (do (play (MockLoopUI. (ref 0) counter [true true true true false]))
-    (should= 5 @counter))))
+    (it "Asks if player wants to go first"
+      (def counter (ref 0))
+      (def ui (MockLoopUI. counter (ref 0) [true true false]))
+      (do (play nil nil ui))
+      (should= 3 @counter))
+
+    (it "Lets human go first if he wants to"
+      (def ref-p1 (ref nil))
+      (def ref-p2 (ref nil))
+      (def ui (MockLoopUI. (ref 0) (ref 0) [true false]))
+
+      (with-redefs [run (fn [p1 p2 & args] (dosync
+                          (ref-set ref-p1 p1)
+                          (ref-set ref-p2 p2)))]
+        (do (play "Human" "Computer" ui)))
+      (should= "Human" @ref-p1)
+      (should= "Computer" @ref-p2))))
 
 (run-specs)
 
--- src/Clojure_TTT/game_loop.clj
+++ src/Clojure_TTT/game_loop.clj
@@ -1,12 +1,14 @@
-(ns clojure_ttt.game_loop)
+(ns clojure_ttt.game_loop
+  (:use clojure_ttt.runner))
 
 (defprotocol LoopUI
   (play-again? [_])
   (go-first? [_]))
 
-(defn play [ui]
+(defn play [human computer ui]
   (loop []
     (go-first? ui)
+    (run human computer)
     (if (play-again? ui)
        (recur))))
 
