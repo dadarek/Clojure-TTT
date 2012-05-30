4d6fb0b84021b64f70b67f793e0e2dfc4a8d2706
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-29 21:27:02 -0500

    Making sure computer injects correct functions to minimax.

--- spec/Clojure_TTT/computer_spec.clj
+++ spec/Clojure_TTT/computer_spec.clj
@@ -2,6 +2,7 @@
   (:use clojure_ttt.runner)
   (:use clojure_ttt.computer)
   (:use clojure_ttt.generic_minimax)
+  (:use [clojure_ttt.minimax_injections :as mmi])
   (:import [clojure_ttt.computer Computer])
   (:use [speclj.core]))
 
@@ -14,7 +15,7 @@
     (with-redefs [score (fn [& args] '(3 3 2))]
       (should= 1 (next-move (Computer. :o) nil))))
 
-  (it "passes in all open moves"
+  (it "passes in all open moves to minimax"
     (let [board '(:x  :o nil
                   nil :x nil
                   nil :o nil)]
@@ -24,12 +25,23 @@
           Exception "#{3 4 6 7 9}"
           (next-move (Computer. :o) board)))))
 
-  (it "passes in its symbol"
+  (it "passes in its symbol to minimax"
     (with-redefs [score (fn [moves player & args]
                           (throw (Exception. (str player))))]
       (should-throw
         Exception ":john"
         (next-move (Computer. :john) nil))))
-)
+
+  (it "injects correct functions to minimax"
+    (with-redefs
+      [score (fn [moves player wins? ties? next-context]
+        (if (not= wins? mmi/wins?) (throw (Exception. "Invalid wins? fn.")))
+        (if (not= ties? mmi/ties?) (throw (Exception. "Invalid ties? fn.")))
+        (if (not= next-context mmi/next-context) (throw (Exception. "Invalid next-context fn.")))
+
+        (throw (Exception. "All Good")))]
+      (should-throw
+        Exception "All Good"
+        (next-move (Computer. :john) nil)))))
 
 (run-specs)
--- src/Clojure_TTT/computer.clj
+++ src/Clojure_TTT/computer.clj
@@ -1,5 +1,6 @@
 (ns clojure_ttt.computer
   (:use clojure_ttt.generic_minimax)
+  (:use clojure_ttt.minimax_injections)
   (:use clojure_ttt.board_utilities)
   (:use clojure_ttt.runner))
 
@@ -7,7 +8,8 @@
   Player
   (next-move [player board]
     (let [empty-squares (get-empty-squares board)
-          scores (score empty-squares x-or-o nil nil nil)
+          scores (score empty-squares x-or-o
+                        wins? ties? next-context)
           top (reduce max scores)]
       (inc (.indexOf scores top))
     )))
