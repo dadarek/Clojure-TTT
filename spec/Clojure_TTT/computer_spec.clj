72f3befe38c4efbd080a0481c50ed5a98a958f25
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-29 21:29:25 -0500

    Cleanup.

--- spec/Clojure_TTT/computer_spec.clj
+++ spec/Clojure_TTT/computer_spec.clj
@@ -6,10 +6,12 @@
   (:import [clojure_ttt.computer Computer])
   (:use [speclj.core]))
 
+(def computer (Computer. :o))
+
 (describe "Computer"
   (it "Returns best scoring moving from minimax"
     (with-redefs [score (fn [& args] '(1 3 2))]
-      (should= 2 (next-move (Computer. :o) nil))))
+      (should= 2 (next-move computer nil))))
 
   (it "Returns first square if several have high score"
     (with-redefs [score (fn [& args] '(3 3 2))]
@@ -23,14 +25,14 @@
                             (throw (Exception. (str moves))))]
         (should-throw
           Exception "#{3 4 6 7 9}"
-          (next-move (Computer. :o) board)))))
+          (next-move computer board)))))
 
   (it "passes in its symbol to minimax"
     (with-redefs [score (fn [moves player & args]
                           (throw (Exception. (str player))))]
       (should-throw
-        Exception ":john"
-        (next-move (Computer. :john) nil))))
+        Exception (str :o)
+        (next-move computer nil))))
 
   (it "injects correct functions to minimax"
     (with-redefs
@@ -42,6 +44,6 @@
           (throw (Exception. "All Good")))]
       (should-throw
         Exception "All Good"
-        (next-move (Computer. :john) nil)))))
+        (next-move computer nil)))))
 
 (run-specs)
