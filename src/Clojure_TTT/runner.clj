13a7ac6bf175ca33198b1e613d01379bb48ce0a8
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-30 11:40:40 -0500

    Reafactoring.

--- src/Clojure_TTT/runner.clj
+++ src/Clojure_TTT/runner.clj
@@ -8,19 +8,17 @@
 (def empty-board '(nil nil nil nil nil nil nil nil nil))
 
 (defn run [x o]
-  (loop [board empty-board
-         player :x]
-    (let [x-won? (winner? (player-squares board :x))
-          o-won? (winner? (player-squares board :o))
-          square-taken? #(not= nil (nth board (dec %)))]
-
-      (if (or (full? board) x-won? o-won?)
-        board
-        (let [current-player (if (= :x player) x o)
-              next-move (next-move current-player board)]
-          (if (square-taken? next-move)
-            (recur board player)
-            (let [new-board (take-square next-move {:board board :player player})
-                  new-player (if (= :x player) :o :x)]
-          (recur new-board new-player))))))))
+  (let [symbol-for #(if (= % x) :x :o)]
+    (loop [board empty-board
+           current-player x]
+      (let [won? #(winner? (player-squares board %))
+            square-taken? #(not= nil (nth board (dec %)))
+            next-player #(if (= x current-player) o x)]
+        (if (or (full? board) (won? :x) (won? :o))
+          board
+          (let [next-move (next-move current-player board)]
+            (if (square-taken? next-move)
+              (recur board current-player)
+              (let [new-board (take-square next-move {:board board :player (symbol-for current-player)})]
+            (recur new-board (next-player))))))))))
 
