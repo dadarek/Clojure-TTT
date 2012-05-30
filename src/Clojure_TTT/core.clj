fd3f23e2275a9cc665f6d951365851dc41609837
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-30 08:12:51 -0500

    Dummy run.

--- src/Clojure_TTT/core.clj
+++ src/Clojure_TTT/core.clj
@@ -1,4 +1,13 @@
-(ns clojure_ttt.core)
+(ns clojure_ttt.core
+  (:use clojure_ttt.runner)
+  (:use clojure_ttt.computer)
+  (:import [clojure_ttt.computer Computer]))
+
+(def computer (Computer. :x))
+
+(def board '(:o  nil :o
+             :o  nil :x
+             :x  nil :x ))
 
 (defn -main []
-  (println "hi"))
+  (next-move computer board))
