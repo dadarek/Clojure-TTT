8b79d73fb1749376edb1d71c349a249103a32dc8
Dariusz Pasciak <dariusz@8thlight.com>
2012-05-29 20:53:28 -0500

    Making sure that first square with "high-score" is returned.

--- spec/Clojure_TTT/computer_spec.clj
+++ spec/Clojure_TTT/computer_spec.clj
@@ -7,7 +7,11 @@
 
 (describe "Computer"
   (it "Returns best scoring moving from minimax"
-    (with-redefs [score (fn [&] '(1 3 2))]
-      (should= 2 (next-move (Computer. :o) nil)))))
+    (with-redefs [score (fn [& args] '(1 3 2))]
+      (should= 2 (next-move (Computer. :o) nil))))
+
+  (it "Returns first square if several have high score"
+    (with-redefs [score (fn [& args] '(3 3 2))]
+      (should= 1 (next-move (Computer. :o) nil)))))
 
 (run-specs)
--- src/Clojure_TTT/computer.clj
+++ src/Clojure_TTT/computer.clj
@@ -1,7 +1,12 @@
 (ns clojure_ttt.computer
+  (:use clojure_ttt.generic_minimax)
   (:use clojure_ttt.runner))
 
 (defrecord Computer [x-or-o]
   Player
-  (next-move [player board] 2))
+  (next-move [player board]
+    (let [result (score nil nil nil nil nil)
+          top (reduce max result)]
+      (inc (.indexOf result top))
+    )))
 
